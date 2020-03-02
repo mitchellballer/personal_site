17140fcb311dbedf8197d6493ddedf8faf5e63f7
Mitchell Baller <mhb91@case.edu>
2020-03-01 20:57:15 -0500

    Resume component

--- angular/personal-site/src/app/app-routing.module.ts
+++ angular/personal-site/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ResumeComponent } from './resume/resume.component';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent},
+  { path: 'projects', component: ProjectsComponent},
+  { path: 'resume', component: ResumeComponent },
 ];
 
 @NgModule({
