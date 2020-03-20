11b8e8c593caaab36e3d153df3215416efc45399
Mitchell Baller <mhb91@case.edu>
2020-03-19 21:14:04 -0400

    passing input to project-card component and displaying them in the card

--- angular/personal-site/src/app/projects/project-card/project-card.component.ts
+++ angular/personal-site/src/app/projects/project-card/project-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project-card',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectCardComponent implements OnInit {
 
+  @Input() name: string;
+  @Input() subTitle: string;
+  @Input() description: string;
   
   constructor() { }
 
